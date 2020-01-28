62f2741e0d115bb975a49c24f782553a3107d1f6
Nikita <itomatpast@gmail.com>
2020-01-28 21:44:24 +0200

    delete and add items

---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './item-add-form.css';
+
+export default class ItemAddForm extends Component{
+    render() {
+        return(
+            <div className="item-add-form">
+                <button
+                    onClick={()=>this.props.onItemAdded('hello')}
+                    className="btn btn-outline-secondary">
+                    Add item
+                </button>
+            </div>
+        )
+    }
+}
